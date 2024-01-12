e915ec9b49d32cc2a0d1e1160e0bdce73b0ec494
Aadarsha Thapa Magar <thapamagaraadarsha@gmail.com>
2024-01-12 09:06:52 +0545

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "wasteDetection",
+    version= '0.0.0',
+    author= 'Gentle Snow',
+    author_email= 'thapamagaraadarsha@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
