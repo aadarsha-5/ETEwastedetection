e34d15b11d05c702b1fdddcfb32eafdc0bfcf3be
Aadarsha Thapa Magar <thapamagaraadarsha@gmail.com>
2024-03-12 07:30:46 +0545

    arduino new code updated

--- arduniocc/arduniocc.ino
+++ arduniocc/arduniocc.ino
@@ -1,7 +1,8 @@
 int in1 = 7;
 int in2 = 8;
 int enA = 9;
-int motorSpeed = 100;
+int motorSpeed = 50;
+int LED = 13;
 
 void setup() {
   // put your setup code here, to run once:
@@ -9,25 +10,27 @@ void setup() {
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(enA, OUTPUT);
+  pinMode(LED, OUTPUT);
   analogWrite(enA, motorSpeed);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (Serial.available() > 0) {
-    char msg = Serial.read();
-
+    int msg = Serial.read();
+    Serial.println(msg);
     if (msg == 'B') {
       digitalWrite(in1, HIGH);
       digitalWrite(in2, LOW);
+      digitalWrite(LED, HIGH);
     } else if (msg == 'N') {
       digitalWrite(in1, LOW);
       digitalWrite(in2, HIGH);
+      digitalWrite(LED, HIGH);
     } else {
       digitalWrite(in1, LOW);
       digitalWrite(in2, LOW);
+      digitalWrite(LED, LOW);
     }
-
-
   }
 }
